b1372622952f16ec7d58ba5730c70f3f64199eb1
Mariusz Czapiewski <m4riusz.cz@gmail.com>
2024-04-18 14:37:36 +0200

    Create default routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: MainComponent },
+  // { path: 'account', component:  },
+  // { path: 'cart', component:  },
+  // { path: 'favorites', component:  },
+  { path: '**', redirectTo: 'home' }
+];
