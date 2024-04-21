c7d1b39cdf986e0f80b754e2a6f348634afe73ce
Mariusz Czapiewski <m4riusz.cz@gmail.com>
2024-04-21 13:24:16 +0200

    Create feature component and set routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
+import { FavoriteComponent } from './favorite/favorite.component';
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: MainComponent },
+  { path: 'favorites', component: FavoriteComponent },
   // { path: 'account', component:  },
   // { path: 'cart', component:  },
-  // { path: 'favorites', component:  },
   { path: '**', redirectTo: 'home' }
 ];
