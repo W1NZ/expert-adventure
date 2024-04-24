c7918045efbf512b770fb6bfeefd430a6438e715
Mariusz Czapiewski <m4riusz.cz@gmail.com>
2024-04-24 17:25:33 +0200

    Create and style cart component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { FavoriteComponent } from './favorite/favorite.component';
+import { CartComponent } from './cart/cart.component';
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 
@@ -7,6 +8,6 @@ export const routes: Routes = [
   { path: 'home', component: MainComponent },
   { path: 'favorites', component: FavoriteComponent },
   // { path: 'account', component:  },
-  // { path: 'cart', component:  },
+  { path: 'cart', component: CartComponent },
   { path: '**', redirectTo: 'home' }
 ];
