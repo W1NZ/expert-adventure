a785f85a80ef2b629e42ad1d1c63036856e09f00
Mariusz Czapiewski <m4riusz.cz@gmail.com>
2024-04-26 18:18:26 +0200

    Create login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { CartComponent } from './cart/cart.component';
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: MainComponent },
   { path: 'favorites', component: FavoriteComponent },
-  // { path: 'account', component:  },
+  { path: 'auth', component: LoginPageComponent },
   { path: 'cart', component: CartComponent },
   { path: '**', redirectTo: 'home' }
 ];
